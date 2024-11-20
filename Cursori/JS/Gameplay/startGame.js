4df6c8ddd46fec0e7ad0c4f3fb57506fd75ff155
CatsaCode <155121307+CatsaCode@users.noreply.github.com>
2024-11-20 08:40:44 -0500

    Delete Cursori
    Moved Cursori to its own repository. In theory, it should still be accessible from catsacode.github.io.
    A project should be its own repository. It was only put in the website in the first place because I was unsure if it would be be accessible from the GitHub Pages website.

--- Cursori/JS/Gameplay/startGame.js
+++
@@ -1,9 +0,0 @@
-function StartGame() {
-    path = new Path(mouse.pos);
-    gameRuntime = 0;
-    score.time = 0;
-    score.risk = 0;
-    gameRunning = true;
-
-    GameScreen.Load(screens.gameplay);
-}
